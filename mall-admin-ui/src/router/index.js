c798b2d0091fa1858dc86218158b06348d48520a
jiakai908 <jiakai908@163.com>
2019-11-07 14:18:13 +0800

    权限列表展示

--- mall-admin-ui/src/router/index.js
+++ mall-admin-ui/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Login from '@/components/views/login'
 import Home from '@/components/views/home'
 import User from '@/components/views/user/user'
+import Right from '@/components/views/roles/right'
 
 Vue.use(Router)
 
@@ -12,11 +13,18 @@ export default new Router({
       name: 'home',
       path: '/',
       component: Home,
-      children: [{
-        name: 'user',
-        path: '/user',
-        component: User
-      }]
+      children: [
+        {
+          name: 'user',
+          path: '/user',
+          component: User
+        },
+        {
+          name: 'right',
+          path: '/right',
+          component: Right
+        }
+      ]
     },
     {
       name: 'login',
